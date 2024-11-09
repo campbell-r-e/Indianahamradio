07fb74f1a0239cfa0da48c4674d3e09b75975385
bsucs <122378640+bsucs@users.noreply.github.com>
2024-11-08 20:53:54 -0500

    d

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,22 @@
 
 export default function Home() {
   return (
+    
+
+
+    
+    
     <div>
-<h1>Welcome to Indiana Ham Radio.com </h1>
+      <h1>Welcome to Indiana Ham Radio.com </h1>
 <br></br>
+
 <p>Soon this Website will have calendars for events in amateur radio thoughout Indiana</p>
 <br></br>
-<div style={{ 
-      backgroundImage: `url("https://militaryalphabet.net/wp-content/uploads/2020/12/HAM-02-1024x681.jpg")` 
-    }}>
-      Hello World
+
     </div>
 
 
-    </div>
+   
 
   
   );
